39399ee6d4796388126ff12d25b07a76ebfab440
k4yp <k4ypos@proton.me>
2023-06-28 21:51:44 +1000

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,6 +9,7 @@ vim.call('plug#begin')
     Plug 'neoclide/coc.nvim'
     Plug 'ryanoasis/vim-devicons'
     Plug 'neovim/nvim-lspconfig'
+    Plug 'rust-lang/rust.vim'
 vim.call('plug#end')
 
 vim.opt.showmatch = true
@@ -24,13 +25,14 @@ set.splitright = true
 set.laststatus = 0
 
 vim.g.NERDTreeMinimalUI=1
+vim.wo.relativenumber = true
 
 vim.api.nvim_set_keymap('n', '<C-d>', ':NERDTreeToggle <bar> set ma <CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-t>', ':vsplit <bar> term<CR> i', {noremap = true})
 vim.api.nvim_set_keymap('n', '<C-q>', ':close<CR>', {noremap = true})
 vim.api.nvim_set_keymap('t', '<C-q>', '<C-\\><C-n><bar>:close<CR>', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-l>', '<C-W>l', {noremap = true})
-vim.api.nvim_set_keymap('n', '<C-h>', '<C-W>h', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-W>l<bar>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-W>h<bar>', {noremap = true})
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n><C-W>h', {noremap = true})
 vim.api.nvim_set_keymap('t', '<C-h>', '<C-\\><C-n><C-W>h', {noremap = true})
 vim.api.nvim_set_keymap('t', '<C-l>', '<C-\\><C-n><C-W>l', {noremap = true})
@@ -50,7 +52,7 @@ augroup END
 vim.api.nvim_command([[
 augroup RunFile
     autocmd filetype python nnoremap <C-e> :w <bar> vsplit <bar> exec 'term python '.shellescape('%')<CR>
-    autocmd filetype rs nnoremap <C-e> :w <bar> vsplit <bar> exec 'term python '.shellescape('%')<CR>
+    autocmd nnoremap <C-e> :w <bar> vsplit <bar> exec 'term cargo run'<CR>
     autocmd filetype cpp nnoremap <C-e> :w <bar> vsplit <bar> exec 'term g++ '.shellescape('%').' -o '.shellescape('%:r').' && ./'.shellescape('%:r')<CR> i
     autocmd filetype c nnoremap <C-e> :w <bar> vsplit <bar> exec 'term gcc '.shellescape('%').' -o '.shellescape('%:r').' && ./'.shellescape('%:r')<CR> i
 augroup END
